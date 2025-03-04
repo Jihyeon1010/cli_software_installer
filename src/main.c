be60abb14ea1ce823cd767a9c30f4a28cf544d4d
Jihyeon Park <pjh06101081@outlook.com>
2025-03-04 12:31:57 +0900

    add some code

--- src/main.c
+++ src/main.c
@@ -3,10 +3,24 @@
 #include <string.h>
 #include <stdbool.h>
 
+// Detect the operating system of the user
+#ifdef _WIN32
+    #define CLEAR "cls"
+    #define INSTALL_CMD(cmd) "powershell -Command \"Start-Process -Verb RunAs -FilePath winget -ArgumentList '" cmd "'\""
+#elif __APPLE__
+    #define CLEAR "clear"
+    #define INSTALL_CMD(cmd) "brew install --cask " cmd
+#elif __linux__
+    #define CLEAR "clear"
+    #define INSTALL_CMD(cmd) "sudo apt update && sudo apt upgrade && sudo apt install -y " cmd
+#else
+    #error "Unsupported OS"
+#endif
+
 // Function of printf Main Commands line interface menu
 void main_menu()
 {
-    system("clear");
+    system(CLEAR);
     printf("\n==== Main Menu ====\n");
     printf("Software\n");
     printf("2. List all contacts\n");
@@ -21,69 +35,7 @@ void main_menu()
 void software_list()
 {
     printf("");
-    
-    printf("Select an option: ");
-}
-
-// Function of prinf programming languages list menu
-void programming_language_list()
-{
-    printf("Programming Languages\n");
-    printf("1. C\n");
-    printf("2. C++\n");
-    printf("3. Java\n");
-    printf("4. Python\n");
-    printf("5. Go\n");
-    printf("6. Ruby\n");
-    printf("7. JavaScript\n");
-    printf("8. PHP\n");
-    printf("9. Swift\n");
-    printf("10. Kotlin\n");
-    printf("11. Rust\n");
-    printf("12. TypeScript\n");
-    printf("13. Dart\n");
-    printf("14. R\n");
-    printf("15. Scala\n");
-    printf("16. Perl\n");
-    printf("17. Lua\n");
-    printf("18. Shell\n");
-    printf("19. Assembly\n");
-    printf("20. Objective-C\n");
-    printf("21. V\n");
-    printf("22. Elixir\n");
-    printf("23. Clojure\n");
-    printf("24. Groovy\n");
-    printf("25. Julia\n");
-    printf("26. Crystal\n");
-    printf("27. Racket\n");
-    printf("28. Erlang\n");
-    printf("29. Haskell\n");
-    printf("30. F#\n");
-    printf("31. OCaml\n");
-    printf("32. Scheme\n");
-    printf("33. Common Lisp\n");
-    printf("34. Prolog\n");
-    printf("35. Smalltalk\n");
-    printf("36. Forth\n");
-    printf("37. COBOL\n");
-    printf("38. Ada\n");
-    printf("39. Fortran\n");
-    printf("40. Pascal\n");
-    printf("41. Logo\n");
-    printf("42. Scratch\n");
-    printf("43. Alice\n");
-    printf("44. ML\n");
-    printf("45. Forth\n");
-    printf("46. BASIC\n");
-    printf("47. Visual Basic\n");
-    printf("48. Delphi\n");
-    printf("49. ActionScript\n");
-    printf("50. ColdFusion\n");
-    printf("51. Eiffel\n");
-    printf("52. ABAP\n");
-    printf("53. Apex\n");
-    printf("54. COBOL\n");
-    printf("55. D\n");
+    printf("\n");
     printf("\n");
     printf("Select an option: ");
 }
@@ -93,6 +45,20 @@ void install_software()
 {
     printf("Installing software...\n");
 }
+
+// List of Web Browsers
+char *web_browsers[] = {
+    "Google Chrome",
+    "Mozilla Firefox",
+    "Opera",
+    "Brave",
+    "Vivaldi",
+    "Microsoft Edge",
+    "Safari",
+    "Tor Browser",
+    "Chromium",
+    "Waterfox"
+};
 
 int main() 
 {
